94d15036510a9c2f4a71c917a58a845bba004a6b
Gregory Brunner <gbrunner@esri.com>
2020-12-31 09:36:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/layers/Layer"
-      ], function (Map, MapView, Layer) {
-        var map = new Map({
-          basemap: "gray-vector"
-        });
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+	  "esri/widgets/Search",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Search) {
 
-        var view = new MapView({
-          map: map,
-          container: "viewDiv",
-          zoom: 7,
-          center: [-90, 40]
-        });
-
-        Layer.fromPortalItem({
-          portalItem: {
-            // autocasts as new PortalItem()
-            /*id: "c0f09a71805f4d45a6b6f3f1e0bdc7fc"*/
-            /*id: "af1ad38816814b7eba3fe74a3b84412d"*/
-            id: "c0238b8ae811443dbe52437496a1a514"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
-
-        // Adds the layer to the map once it loads
-        function addLayer(layer) {
-          map.add(layer);
-        }
-
-        function rejection(error) {
-          console.log("Layer failed to load: ", error);
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"fbbbaa2fbfda41b8b3f96427c3ac5c79" 
         }
       });
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+		viewingMode:'local'
+      });
+	  
+	  var searchWidget = new Search({
+        view: view
+      });
+
+      // Add the search widget to the top right corner of the view
+      view.ui.add(searchWidget, {
+        position: "top-right"
+      });
+
+    });
